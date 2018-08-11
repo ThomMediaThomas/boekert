fdc2aabdd250610197582942125179c24b03b3b3
Thomas <thomas.bartels@nos.nl>
2018-08-11 18:54:36 +0100

    Added types

---
+++ database/migrations/2018_08_11_175253_create_accomodation_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccomodationTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accomodation_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accomodation_types');
+    }
+}
