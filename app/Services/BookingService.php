89a2c4d4acf358225e261ff0fac4f0a47d550cf7
Thomas <thomas.bartels@nos.nl>
2018-08-11 19:25:58 +0100

    Created bookingService

---
+++ app/Services/BookingService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class BookingService
+{
+    /**
+     * @return string
+     */
+    public function generateBoekertId ()
+    {
+        $now = Carbon::now();
+        $randomizer = Str::random(6);
+        return 'B_' . $now . '_' . $randomizer;
+    }
+}
