b42a838c7ddc498eb31e4c0325aaf101677e594f
Thomas <thomas.bartels@nos.nl>
2018-08-16 08:17:08 +0100

    Small fix for js-logic (2)

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         autoClose: true
     });
 
-    if ($('#create-booking') || $('#create-accomodation')) {
+    if ($('#create-booking').length > 0 || $('#create-accomodation').length > 0) {
         $('select#type').on('change', function () {
             $('.input-field.show-on-change-type').hide();
             $('#show-for-' + this.value).show();
