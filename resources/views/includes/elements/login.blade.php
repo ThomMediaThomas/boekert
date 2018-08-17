0592314c83b87de6f71c7eb7f89bae1ea4f4eadf
Thomas <thomas.bartels@nos.nl>
2018-08-17 07:49:30 +0100

    B0020: Inloggen met gebruikersnaam

--- resources/views/includes/elements/login.blade.php
+++ resources/views/includes/elements/login.blade.php
@@ -6,10 +6,10 @@
             <p>Hartelijk welkom bij <strong>boekert</strong>. Vul hieronder je gegevens in om in te loggen.</p>
             <div class="input-field">
                 <i class="material-icons prefix">account_circle</i>
-                <label for="email">E-mailadres</label>
-                <input id="email" type="email" class="{{ $errors->has('email') ? ' invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-                @if ($errors->has('email'))
-                    <span class="helper-text" data-error="wrong">{{ $errors->first('email') }}</span>
+                <label for="username">Gebruikersnaam</label>
+                <input id="username" type="text" class="{{ $errors->has('username') ? ' invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+                @if ($errors->has('username'))
+                    <span class="helper-text" data-error="wrong">Dit veld is niet juist.</span>
                 @endif
             </div>
 
@@ -18,7 +18,7 @@
                 <label for="password">Wachtwoord</label>
                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' invalid' : '' }}" name="password" required>
                 @if ($errors->has('password'))
-                <span class="helper-text" data-error="wrong">{{ $errors->first('password') }}</span>
+                <span class="helper-text" data-error="wrong">Dit veld is niet juist.</span>
                 @endif
             </div>
 
