f660dfd2d64e50b7d500cfa391d47a70d2b2a4ba
Thomas <thomas.bartels@nos.nl>
2018-08-27 13:15:27 +0100

    B0077: pagetitle toevoegen

--- resources/views/includes/document_start.blade.php
+++ resources/views/includes/document_start.blade.php
@@ -3,6 +3,7 @@
 <head>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::asset('styles/main.css') }}">
+    <title>BOEKERT | boekingen beheren</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
