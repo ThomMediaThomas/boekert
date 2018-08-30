518dcb54fcabbeaca5fed1628b9c2a1b80ee5887
Thomas <thomas.bartels@nos.nl>
2018-08-30 07:54:28 +0100

    B0081, B0082: datepicker verbeteringen

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
     initSidenav();
-
-    $('select').formSelect();
-    $('.datepicker').datepicker({
-        format: 'dd-mm-yyyy',
-        autoClose: true
-    });
-    $('table.sortable').tablesorter();
-
-    if ($('#create-booking').length > 0 || $('#create-accommodation').length > 0) {
-        $('select#type').on('change', function () {
-            $('.input-field.show-on-change-type').hide();
-            $('#show-for-' + this.value).show();
-        }).trigger('change');
-    }
-
-    if($('.alert').length > 0) {
-        $('.alert').on('click', function () {
-            $(this).fadeOut();
-        });
-    }
-
-    if($('#calendar')) {
-        var calendar = new Calendar();
-        calendar.init();
-    }
+    initForms();
+    initTables();
+    initAlerts();
+    initCalendar();
 });
 
 var SIDENAV_OPEN = true;
-function initSidenav()
-{
+function initSidenav() {
     $('.sidenav').sidenav();
     $('#collapse-sidebar').on('click', function () {
         $('header, main, footer, #collapse-sidebar').toggleClass('sidebar-closed');
@@ -42,4 +20,47 @@ function initSidenav()
 
         SIDENAV_OPEN = !SIDENAV_OPEN;
     })
+}
+
+function initForms() {
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+        format: 'dd-mm-yyyy',
+        autoClose: true,
+        firstDay: 1,
+        onSelect: function (value)  {
+            var currentName = this.$el.attr('name'),
+                $dateTo = $('input.datepicker[name="date_to"]');
+
+            if (currentName == 'date_from' && $dateTo.length > 0) {
+                $dateTo.datepicker('setDate', value);
+            }
+        }
+    });
+
+    if ($('#create-booking').length > 0 || $('#create-accommodation').length > 0) {
+        $('select#type').on('change', function () {
+            $('.input-field.show-on-change-type').hide();
+            $('#show-for-' + this.value).show();
+        }).trigger('change');
+    }
+}
+
+function initTables() {
+    $('table.sortable').tablesorter();
+}
+
+function initAlerts() {
+    if ($('.alert').length > 0) {
+        $('.alert').on('click', function () {
+            $(this).fadeOut();
+        });
+    }
+}
+
+function initCalendar() {
+    if ($('#calendar')) {
+        var calendar = new Calendar();
+        calendar.init();
+    }
 }
